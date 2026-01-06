9126d50a6bb9c4bb057f233b56ab0df2e56f64ee
Mathias Jespersen <mathiasjespersen@gmail.com>
2026-01-06 09:27:30 +0100

    create article page

--- studio/src/structure/index.ts
+++ studio/src/structure/index.ts
@@ -1,14 +1,5 @@
 import {CogIcon, DocumentTextIcon} from '@sanity/icons'
 import type {StructureBuilder, StructureResolver} from 'sanity/structure'
-import pluralize from 'pluralize-esm'
-
-/**
- * Structure builder is useful whenever you want to control how documents are grouped and
- * listed in the studio or for adding additional in-studio previews or content to documents.
- * Learn more: https://www.sanity.io/docs/structure-builder-introduction
- */
-
-const DISABLED_TYPES = ['settings', 'assist.instruction.context']
 
 export const structure: StructureResolver = (S: StructureBuilder) =>
   S.list()
