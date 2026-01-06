093211b5a5d2dab9f1d2dc4aa51e11eb8921e9f0
brille-bjarke <hello@brillebjarke.dk>
2026-01-06 13:51:18 +0100

    Removed mux player theme color + added aspect-ratio to video tag

--- frontend/app/components/Visual.tsx
+++ frontend/app/components/Visual.tsx
@@ -5,24 +5,29 @@ import {dataset, projectId} from '@/sanity/lib/api'
 import MuxPlayer from "@mux/mux-player-react";
 
 export default function Visual({visual}: {visual: Visual}) {
+    console.log('Visual component received visual:', visual);
     return (
         <>
             {
-                visual.mediaType === 'image' && visual.image && visual.image.asset?._ref && (
-                    <SanityImage
-                        baseUrl={`https://cdn.sanity.io/images/${projectId}/${dataset}/`}
-                        id={visual.image.asset._ref}
-                        {...visual.image}
-                    />
-                )
+            visual.mediaType === 'image' && visual.image && visual.image.asset?._ref && (
+                <SanityImage
+                    className='w-full'
+                    baseUrl={`https://cdn.sanity.io/images/${projectId}/${dataset}/`}
+                    id={visual.image.asset._ref}
+                    {...visual.image}
+                />
+            )
             }
             {
-                visual.mediaType === 'video' && visual.video && (visual.video.asset as any)?.playbackId && (
-                    <MuxPlayer
-                        playbackId={(visual.video.asset as any)?.playbackId}
-                        accentColor="#ea580c"
-                    />
-                )
+            visual.mediaType === 'video' && visual.video && (visual.video.asset as any)?.playbackId && (
+                <MuxPlayer
+                    className='w-full'
+                    style={{
+                        aspectRatio: (visual.video.asset as any)?.data.aspect_ratio.replace(':', '/') || '16/9'
+                    }}
+                    playbackId={(visual.video.asset as any)?.playbackId}
+                />
+            )
             }
         </>
     )
